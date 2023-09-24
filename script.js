74d6755f5600e27c7a51b6d96ec866c4da0bf0ae
Niel-Andrade <nielandrade10@gmail.com>
2023-09-23 22:51:16 -0300

    Version 1.0 Jokenpo!

--- script.js
+++ script.js
@@ -1,27 +1,36 @@
 let selectedOption = null;
 let numberRandom, numberChosen = null;
 let img = document.querySelector("#questionMark")
+let selectedOptionName = null;
+
+function selectOptionName(optionNumber){
+    if (selectedOptionName !== null) {
+        selectedOptionName.classList.remove('selectedName');
+    }
+
+    selectedOptionName = document.getElementById('nameOption' + optionNumber);
+    selectedOptionName.classList.add('selectedName');
+}
 
 function selectOption(optionNumber) {
     if (selectedOption !== null) {
-        // Remove a classe 'selected' da div selecionada anteriormente
+ 
         selectedOption.classList.remove('selected');
     }
 
-    // Seleciona a nova div
     selectedOption = document.getElementById('option' + optionNumber);
     selectedOption.classList.add('selected');
 
-    // Habilita o botão quando uma div é selecionada
     document.getElementById('confirm-button').disabled = false;
 }
+//ORIENTAÇÃO:
 // 0 = PEDRA
 // 1 = PAPEL
 // 2 = TESOURA
 function buttonClicked() {
     if (selectedOption === null) {
         alert("Escolha uma opção antes de confirmar.");
-        return; // Evita continuar se nenhuma opção foi escolhida
+        return; 
     }
 
     numberRandom = Math.floor(Math.random() * 3);
@@ -49,27 +58,31 @@ function buttonClicked() {
     }
     console.log(numberChosen);
     console.log(numberRandom);
-
-    if ((numberChosen === 0 && numberRandom === 2) || (numberChosen === 2 && numberRandom === 1) || (numberChosen === 1 && numberRandom === 0)) {
-        console.log("Você Ganhou");
-        showAnimation("win-animation");
-    } else if ((numberChosen === 0 && numberRandom === 1) || (numberChosen === 1 && numberRandom === 2) || (numberChosen === 2 && numberRandom === 0)) {
-        console.log("Você Perdeu");
-        showAnimation("lose-animation");
-    } else {
-        console.log("Empate");
-        showAnimation("draw-animation");
-    }
+    
+    setTimeout(() => {
+        if ((numberChosen === 0 && numberRandom === 2) || (numberChosen === 2 && numberRandom === 1) || (numberChosen === 1 && numberRandom === 0)) {
+            console.log("Você Ganhou");
+            showAnimation("win-animation");
+        } else if ((numberChosen === 0 && numberRandom === 1) || (numberChosen === 1 && numberRandom === 2) || (numberChosen === 2 && numberRandom === 0)) {
+            console.log("Você Perdeu");
+            showAnimation("lose-animation");
+        } else {
+            console.log("Empate");
+            showAnimation("draw-animation");
+        }
+    }, 1000);
 }
 
 function showAnimation(animationId) {
     const animation = document.getElementById(animationId);
-    
-    // Defina a opacidade como 1 apenas quando necessário
-    animation.style.opacity = 0.5;
+    const vs = document.getElementById('vs');
+   
 
-    // Limpar a animação após alguns segundos (opcional)
+    animation.style.display = "flex";
+    vs.style.opacity = 0
+
     setTimeout(() => {
-        animation.style.opacity = 0;
-    }, 1000); // Defina o tempo em milissegundos (3 segundos neste exemplo)
+        animation.style.display = "none"
+        vs.style.opacity = 1
+    }, 2000);
 }
